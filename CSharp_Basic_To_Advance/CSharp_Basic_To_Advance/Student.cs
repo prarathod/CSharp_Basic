a5ec47b0b7bb43ec22919675aba84958f048f371
prarathod <itsmeprajwal2@gmail.com>
2022-06-14 11:49:15 +0530

    Class Prediction according to age.

---
+++ CSharp_Basic_To_Advance/CSharp_Basic_To_Advance/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CSharp_Basic_To_Advance
+{
+       
+    internal class Student
+    {
+        string name;
+        int age;
+        // creating constructor.
+        public Student(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+           
+        }
+         public void getInfo()
+        {
+            
+            int std = 0;
+            if (this.age > 6)
+            {
+                std = this.age- 6;
+            }
+            
+            Console.WriteLine("Name: {0} and age is {1}\n According to age the class is {2}",this.name,this.age,std);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("************* School Software **************");
+            Student s1 = new Student("Ram", 20);
+            s1.getInfo();
+        }
+    }
+}
