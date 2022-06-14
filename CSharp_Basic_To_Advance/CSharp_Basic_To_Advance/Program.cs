567a673b14b961b2378da078b84539ff97004be3
prarathod <itsmeprajwal2@gmail.com>
2022-06-14 11:32:13 +0530

    First commint

---
+++ CSharp_Basic_To_Advance/CSharp_Basic_To_Advance/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp_Basic_To_Advance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
