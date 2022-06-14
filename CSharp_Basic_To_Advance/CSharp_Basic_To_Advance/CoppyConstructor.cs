fb03a0f9862cf51fcae36035b387c774007b8162
prarathod <itsmeprajwal2@gmail.com>
2022-06-14 13:12:43 +0530

    coppy constractor

---
+++ CSharp_Basic_To_Advance/CSharp_Basic_To_Advance/CoppyConstructor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Basic_To_Advance
+{
+    internal class CoppyConstructor
+    {
+        string name;
+        int age;
+        bool dis;
+
+        public CoppyConstructor(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public CoppyConstructor(CoppyConstructor e, bool val)
+        {
+            this.name = e.name;
+            this.age = e.age;
+            this.dis = val;
+        }
+        
+        public static void main()
+        {
+            CoppyConstructor s1 = new CoppyConstructor("ram", 23);
+
+            Console.WriteLine(s1);
+
+        }
+    }
+}
