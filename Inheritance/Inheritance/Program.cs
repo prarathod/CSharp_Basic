f2106c49860cf98db274f26ef049e3901516bb8a
prarathod <itsmeprajwal2@gmail.com>
2022-06-15 11:04:07 +0530

    Inheritance

---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class BaseClass 
+    { 
+        public void Show1()
+        {
+            Console.WriteLine("Output from Show1");
+        }
+    }
+
+    class DerivedClass:BaseClass
+    {
+        public void Show2()
+        {
+            Console.WriteLine("Output from Show2");
+        }
+    }
+
+
+    internal class Program
+    {
+        
+       
+        static void Main(string[] args)
+        {
+            DerivedClass obj1 = new DerivedClass();
+            obj1.Show1();
+            obj1.Show2();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
