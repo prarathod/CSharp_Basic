f8180151186ce008d596c9282244e2aa8847ca73
prarathod <itsmeprajwal2@gmail.com>
2022-06-20 10:46:39 +0530

    data first approch

---
+++ DataBaseFirst/DataBaseFirst/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBaseFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new CodeFirsy;
+            var post = new POST
+            {
+                Body ="Body",
+                Data
+            }
+        }
+    }
+}
---
+++ MVCDatabaseFirstDemo/MVCDatabaseFirstDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCDatabaseFirstDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var databaseobj = new DemoDatabaseFirstEntities();
+            var post = new Post
+            {
+                PublishDate = DateTime.Now,
+                Title = "Test Title",
+                Body = "test Body"
+            };
+            databaseobj.Posts.Add(post);
+            databaseobj.SaveChanges();
+        }
+    }
+}
