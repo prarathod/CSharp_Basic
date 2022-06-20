6cbb4d33a6152a4358031778e8adf0bf07f0c0ab
prarathod <itsmeprajwal2@gmail.com>
2022-06-20 12:44:54 +0530

    complited database frist approach

---
+++ DemoDatabaseFirst2/DemoDatabaseFirst2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoDatabaseFirst2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PlutoDbContext _db = new PlutoDbContext();
+            var courseList = _db.GetCourses();
+
+            
+
+            foreach(var elm  in courseList)
+            {
+                Console.WriteLine(elm.Title);
+            }
+            Console.ReadLine();
+        }
+    }
+}
