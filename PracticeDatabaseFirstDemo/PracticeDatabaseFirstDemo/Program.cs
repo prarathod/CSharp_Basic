615c00d8385bd98a6688b2e7e21c7f2e527f5550
prarathod <itsmeprajwal2@gmail.com>
2022-06-20 11:07:56 +0530

    Practice Database first demo

---
+++ PracticeDatabaseFirstDemo/PracticeDatabaseFirstDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeDatabaseFirstDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var databaseobj = new DemoDatabaseFirstEntities();
+            var post = new Post
+            {
+                PublishDate = DateTime.Now,
+                Title = "second Test title",
+                Body = "Second body test"
+            };
+            databaseobj.Posts.Add(post);
+            databaseobj.SaveChanges();
+        }
+    }
+}
