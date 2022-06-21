acc150b32f6ec5deef041f68a8d72bdb0d975850
prarathod <itsmeprajwal2@gmail.com>
2022-06-21 16:20:13 +0530

    addintion

--- EmployeeDBConsoleApp/EmployeeDBConsoleApp/Program.cs
+++ EmployeeDBConsoleApp/EmployeeDBConsoleApp/Program.cs
@@ -30,23 +30,49 @@ namespace EmployeeDBConsoleApp
 
             Console.WriteLine("Your Name is {0} and Age is {1}, saved succesfully", name, age);
         }
+
+        public void ShowDatabase()
+        {
+            EmployeeEntities db = new EmployeeEntities();
+
+            foreach (EmployeeTable table in db.EmployeeTables)
+            {
+                Console.WriteLine("{0} {1} {2}", table.Id, table.EmployeeName, table.EmployeeAge);
+            }
+        }
+
+        
         static void Main(string[] args)
         {
             Console.WriteLine("********* Employee Detail App **********");
-
             Program p1 = new Program();
             bool repeat = true;
             do {
-               
-                p1.AddEmployeeData();
-                Console.Write("Do you want to add Data again if yes press Y :- ");
-               string repeatVal = Console.ReadLine().Trim().ToLower();
+                Console.Write("To see the database Enter S and to add employee Enter A:- ");
+                string input = Console.ReadLine().Trim().ToLower();
+
+                if (input == "s")
+                {
+                    p1.ShowDatabase();
+                }
+                else if (input == "a")
+                {
+                    p1.AddEmployeeData();
+                }
+                else
+                {
+                    Console.WriteLine("Thank you for visiting");
+                }
+
+                Console.Write("Do you want to run program again if yes press Y :- ");
+                string repeatVal = Console.ReadLine().Trim().ToLower();
                 if (repeatVal != "y")
                 {
                     repeat = false;
                 };
 
             } while (repeat);
+
 
         }
     }
