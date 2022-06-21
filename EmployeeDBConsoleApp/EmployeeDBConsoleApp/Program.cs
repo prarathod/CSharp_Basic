5d8b1b00ae0c6b992b52eeb71b6f915efd235c8f
prarathod <itsmeprajwal2@gmail.com>
2022-06-21 11:38:12 +0530

    EmployeeDBConsoleApp

---
+++ EmployeeDBConsoleApp/EmployeeDBConsoleApp/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeDBConsoleApp
+{
+    internal class Program
+
+    {
+
+        public void AddEmployeeData()
+        {
+            Console.Write("Enter Employee Name:- ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter Employee Name:- ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+
+
+            EmployeeTable table = new EmployeeTable();
+            table.EmployeeName = name;
+            table.EmployeeAge = age;
+
+            EmployeeEntities db = new EmployeeEntities();
+            db.EmployeeTables.Add(table);
+            db.SaveChanges();
+
+            Console.WriteLine("Your Name is {0} and Age is {1}, saved succesfully", name, age);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("********* Employee Detail App **********");
+
+            Program p1 = new Program();
+            bool repeat = true;
+            do {
+               
+                p1.AddEmployeeData();
+                Console.Write("Do you want to add Data again if yes press Y :- ");
+               string repeatVal = Console.ReadLine().Trim().ToLower();
+                if (repeatVal != "y")
+                {
+                    repeat = false;
+                };
+
+            } while (repeat);
+
+        }
+    }
+}
