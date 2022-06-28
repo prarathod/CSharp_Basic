41488f32fbc4aefa574f82e03fd2c83762290ffc
prarathod <itsmeprajwal2@gmail.com>
2022-06-28 15:18:06 +0530

    HotelListing1

---
+++ HotelListing1/HotelListing.API.1/Data/HotelListingDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.API._1.Data
+{
+    public class HotelListingDbContext :DbContext
+    {
+        public HotelListingDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Country> Countries { get; set; }
+
+
+    };
+};
