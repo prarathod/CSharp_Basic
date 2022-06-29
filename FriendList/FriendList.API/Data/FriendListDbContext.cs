57363cefd2c7342dd8e5f210ced034d246c095c6
prarathod <itsmeprajwal2@gmail.com>
2022-06-29 12:15:24 +0530

    friendlist

---
+++ FriendList/FriendList.API/Data/FriendListDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace FriendList.API.Data
+{
+    public class FriendListDbContext : DbContext
+    {
+        public FriendListDbContext(DbContextOptions options) : base(options) { }
+       
+    };
+    
+}
---
+++ FriendList/FriendList.API/Data/Infofnd.cs
@@ -0,0 +1,9 @@
+﻿namespace FriendList.API.Data
+{
+    public class Infofnd
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int age { get; set; }
+    }
+}
