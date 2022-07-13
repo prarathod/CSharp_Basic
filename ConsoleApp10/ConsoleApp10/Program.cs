05a79d23178f6e457efcf8faf8956d73fef14ff8
prarathod <itsmeprajwal2@gmail.com>
2022-07-13 11:44:54 +0530

    user input array creating

---
+++ ConsoleApp10/ConsoleApp10/Program.cs
@@ -0,0 +1,18 @@
+﻿
+Console.WriteLine("******** Program To Create Array on User Side *********");
+
+Console.Write("Enter The legth Of Array:- ");
+int num = int.Parse(Console.ReadLine());
+int[] numArray = new int[num];
+
+for (int i = 0; i < num; i++)
+{
+    Console.Write("Enter Value on Index "+(i+1)+" :- ");
+    numArray[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("***** Your array is ********");
+foreach(int elm in numArray)
+{
+    Console.Write(elm +" ");
+}
