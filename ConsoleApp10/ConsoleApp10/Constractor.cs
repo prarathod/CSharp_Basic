6153722cca6bcd5157f952002f43401cc7b643c0
prarathod <itsmeprajwal2@gmail.com>
2022-07-15 11:00:04 +0530

    constructor

---
+++ ConsoleApp10/ConsoleApp10/Constractor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    public class Constractor
+    {
+        int age;
+        public Constractor(int age)
+        {
+            this.age = age;
+            Console.WriteLine("Age data from constructor: "+this.age);
+        }
+        public void Show()
+        {
+            Console.WriteLine(age);
+        }
+
+    }
+}
--- ConsoleApp10/ConsoleApp10/Program.cs
+++ ConsoleApp10/ConsoleApp10/Program.cs
@@ -1,15 +1,12 @@
 ﻿
 using ConsoleApp10;
 using static ConsoleApp10.Class1;
+using static ConsoleApp10.Constractor;
+Console.WriteLine("******** Program To Experiment *********");
 
-Console.WriteLine("******** Program To Create Array on User Side *********");
-
-Console.Write("Enter The legth Of Array:- ");
-
-Class1 p1 = new Class1();
-p1.setStudent(23, "Prajwal");
-
-p1.getStudent();
-
+Constractor C1 = new Constractor(23);
+Constractor C2 = new Constractor(25);
+C1.Show();
+Console.WriteLine("***");
+C2.Show();
 Console.ReadLine();
-
