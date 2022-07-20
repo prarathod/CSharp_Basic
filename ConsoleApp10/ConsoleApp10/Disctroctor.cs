a2cc8d57615062d825e16ae25fd1a5430219aa33
prarathod <itsmeprajwal2@gmail.com>
2022-07-20 12:09:56 +0530

    ConsoleApp2

---
+++ ConsoleApp10/ConsoleApp10/Disctroctor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    public class Disctroctor
+    {
+        public string name;
+        public int age;
+        public int distructorCount = 1;
+        public Disctroctor(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public string ShowName()
+        {
+            return name;
+
+        }
+        public int ShowAge()
+        {
+            return age;
+        }
+
+        ~Disctroctor()
+        {
+            Console.WriteLine("Distructor involked {0}",distructorCount);
+            distructorCount++;
+        }
+    }
+}
--- ConsoleApp10/ConsoleApp10/Program.cs
+++ ConsoleApp10/ConsoleApp10/Program.cs
@@ -2,11 +2,19 @@
 using ConsoleApp10;
 using static ConsoleApp10.Class1;
 using static ConsoleApp10.Constractor;
+using static ConsoleApp10.Disctroctor;
+using Viklele.CRUD.AuthorizationValidators;
+
+
 Console.WriteLine("******** Program To Experiment *********");
 
-Constractor C1 = new Constractor(23);
-Constractor C2 = new Constractor(25);
-C1.Show();
-Console.WriteLine("***");
-C2.Show();
-Console.ReadLine();
+Disctroctor p1 = new Disctroctor("Prajwal", 23);
+Console.WriteLine(p1.ShowAge());
+Console.WriteLine(p1.ShowName());
+
+BaseAuthorizationValidator.Equals
+
+Disctroctor p2 = new Disctroctor("ram", 24);
+Console.WriteLine(p2.ShowAge());
+Console.WriteLine(p2.ShowName());
+
